98169038211433db0e888296261500610e9801bc
DavidGomez3 <71481274+DavidGomez3@users.noreply.github.com>
2024-08-16 22:49:39 -0500

    Pasito a pasito vamos aprendiendo

--- aprender-js-ts/src/test.js
+++ aprender-js-ts/src/test.js
@@ -1,1 +1,16 @@
-let variable = 2; 
+let variable = 2;
+
+document.addEventListener("DOMContentLoaded", function () {
+  let navItems = document.querySelectorAll(".nav_item");
+  console.log(navItems.length);
+  navItems.forEach((item) => {
+    item.addEventListener("click", clicNavegacion);
+  });
+
+  function clicNavegacion(event) {
+    navItems.forEach((element) => {
+      element.classList.remove("active");
+    });
+    event.currentTarget.classList.add("active");
+  }
+});
